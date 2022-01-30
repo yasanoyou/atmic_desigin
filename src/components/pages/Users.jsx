41630f4c703f8dde63908f904bc84bedc05aad6d
yasanoyou <wearejanne2007@icloud.com>
2022-01-31 03:25:23 +0900

    再レンダリングの最適化

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,7 +1,9 @@
+import React, { useContext } from "react";
 import styled from "styled-components"
 import { SearchInput } from "../molecules/SearchInput";
 import {UserCard} from "../../components/organisms/user/UserCard";
-import { useLocation } from "react-router-dom";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { UserContext } from "../../providers/UserProvider";
 
 const users = [...Array(10).keys()].map(((val)=>{
   return {
@@ -18,10 +20,14 @@ const users = [...Array(10).keys()].map(((val)=>{
 }))
 
 export const Users = () => {
+  const {userInfo, setUserInfo} = useContext(UserContext);
+  const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
   return(
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
