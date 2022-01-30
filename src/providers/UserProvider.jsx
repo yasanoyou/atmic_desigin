b9a87979ca397eecff4254e07a0df36d4c5c48fd
yasanoyou <wearejanne2007@icloud.com>
2022-01-31 02:57:27 +0900

    globalなstate管理

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,13 +1,15 @@
-import React, { createContext } from "react"
+import React, { createContext, useState } from "react"
 
 export const UserContext = createContext({});
 
 export const UserProvider = (props) => {
   const { children } = props;
-  const contextName = "YUKI";
+
+  const [userInfo, setUserInfo] = useState(null);
+
   return(
-    <UserContext.Provider value={{ contextName }}>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </UserContext.Provider>
   ); 
-}
+};
